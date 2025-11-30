55c36695444b547b9b97a164bba43090f13a4d8c
Aayushi-Sinha <122288743+Aayushi-Sinha@users.noreply.github.com>
2025-12-01 01:25:04 +0530

    Update api.js

--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -1,2 +1,2 @@
-export const API_URL = "https://wallet-api-cxqp.onrender.com/api";
+export const API_URL = "https://rn-wallet-v025.onrender.com/api/health";
 // export const API_URL = "http://localhost:5001/api";
